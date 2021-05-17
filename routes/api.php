92d1e774cd600f0dbcc400ec814fc1b49ec42dfa
Lindomar <lindomar1016@hotmail.com>
2021-05-17 17:10:47 -0300

    Feat: Usuarios

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::resource('produtos','ProdutosController');
+Route::resource('users','UsersController');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
